74a9fcfc0cd79e86d2e4e7eaa764d2dc712938a5
chrismartinez450 <christopher.martinez.450@my.csun.edu>
2021-04-26 14:34:09 -0700

    added bank page and changed some useless code.

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 from tkinter import *
 
-root = Tk()
-
-
-def register_page(root):
-    page = Frame(root)
-    page.grid()
-    title_lbl = Label(root, text="Registration page").grid(row=0, column=0, columnspan=2, pady=5)
-    f_name_lbl = Label(root, text="First name: ").grid(row=1, column=0, padx=30)
-    f_name_entry = Entry(root).grid(row=1,column=1)
-
-    last_name_lbl = Label(root, text="Last name: ").grid(row=2, column=0)
-    last_name_entry = Entry(root).grid(row=2, column=1, pady=10)
-
-    login_id_lbl = Label(root, text="Login ID: ").grid(row=3, column=0)
-    login_id_entry = Entry(root).grid(row=3, column=1, pady=10)
-
-    password_lbl = Label(root, text="Password: ").grid(row=4, column=0)
-    password_entry = Entry(root).grid(row=4, column=1, pady=10)
-
-    next_button = Button(root, text="Next", width=10).grid(row=5,column=1)
-    back_button = Button(root, text="Back", width=10, command=change_to_login_page).grid(row=5, column=0)
-
-
-def change_to_reg_page():
-    for widget in root.winfo_children():
-        widget.destroy()
-    register_page(root)
+tk = Tk()
 
 
 def login_page(root):
@@ -39,10 +13,55 @@ def login_page(root):
 
 
 def change_to_login_page():
-    for widget in root.winfo_children():
+    for widget in tk.winfo_children():
         widget.destroy()
-    login_page(root)
+    login_page(tk)
 
 
-login_page(root)
-root.mainloop()
+def register_page():
+    title_lbl = Label(tk, text="Registration page").grid(row=0, column=0, columnspan=2, pady=5)
+    f_name_lbl = Label(tk, text="First name: ").grid(row=1, column=0, padx=30)
+    f_name_entry = Entry(tk).grid(row=1, column=1)
+
+    last_name_lbl = Label(tk, text="Last name: ").grid(row=2, column=0)
+    last_name_entry = Entry(tk).grid(row=2, column=1, pady=10)
+
+    login_id_lbl = Label(tk, text="Login ID: ").grid(row=3, column=0)
+    login_id_entry = Entry(tk).grid(row=3, column=1, pady=10)
+
+    password_lbl = Label(tk, text="Password: ").grid(row=4, column=0)
+    password_entry = Entry(tk).grid(row=4, column=1, pady=10)
+
+    next_button = Button(tk, text="Next", width=10, command=change_to_add_bank_page).grid(row=5, column=1)
+    back_button = Button(tk, text="Back", width=10, command=change_to_login_page).grid(row=5, column=0)
+
+
+def change_to_reg_page():
+    for widget in tk.winfo_children():
+        widget.destroy()
+    register_page()
+
+
+def add_bank_page():
+    bank_title_lbl = Label(tk, text="Please add bank info").grid(row=0, column=0, columnspan=2, pady=5)
+    bank_name_lbl = Label(tk, text="Bank Name: ").grid(row=1, column=0, padx=30)
+    bank_name_entry = Entry(tk).grid(row=1, column=1, pady=10)
+
+    account_num_lbl = Label(tk, text="Account number: ").grid(row=2, column=0)
+    account_num_entry = Entry(tk).grid(row=2, column=1, pady=10)
+
+    zipcode_lbl = Label(tk, text="Zipcode: ").grid(row=3, column=0)
+    zipcode_entry = Entry(tk).grid(row=3, column=1, pady=10)
+
+    next_button2 = Button(tk, text="Next", width=10).grid(row=4, column=1)
+    back_button2 = Button(tk, text="Back", width=10, command=change_to_reg_page).grid(row=4, column=0)
+
+
+def change_to_add_bank_page():
+    for widget in tk.winfo_children():
+        widget.destroy()
+    add_bank_page()
+
+
+login_page(tk)
+tk.mainloop()
